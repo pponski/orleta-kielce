686905a1b386ef903c45417df8763a299a3d7138
pponski <pponski@MacBook-Pro-pponski.local>
2023-05-04 07:42:34 +0200

    fix small article

--- src/components/SmallArticle.js
+++ src/components/SmallArticle.js
@@ -18,14 +18,14 @@ const SmallArticle = ({title, slug, image, shortDescription}) => {
   return (
     <div className={styles}>
       <Link href={slug} className=''>
-      {img && (<Image src={image.url} width="340" height="200" className='hidden md:block rounded-md h-40 object-cover object-center md:group-hover:object-bottom transition-all duration-[2s] linear'></Image>)}
+      <Image src={image.url} width="340" height="200" className='hidden md:block rounded-md h-40 object-cover object-center md:group-hover:object-bottom transition-all duration-[2s] linear'></Image>
       </Link>
       <div>
         <Link href={slug}>
         <p class="text-base font-bold group-hover:text-green-700">{title}</p>
         </Link>
       <Link href={slug}>
-      {shortDesc && (<p class="text-xs pb-1 max-w-xl font-normal">{shortDescription}</p>)}
+      {shortDescription && (<p class="text-xs pb-1 max-w-xl font-normal">{shortDescription}</p>)}
       </Link>
       <div className="info flex flex-col items-start lg:flex-row lg:gap-4 lg:items-center">
     {/* <div className="categories">
