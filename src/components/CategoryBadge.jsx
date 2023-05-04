2bdab7b243f210080a657fe4e2f89232067089b8
pponski <pponski@MacBook-Pro-pponski.local>
2023-05-04 07:25:26 +0200

    delete 500 page error

--- src/components/CategoryBadge.jsx
+++ src/components/CategoryBadge.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const CategoryBadge = () => {
   return (
-    <Link href="/seniorzy">
+    <Link href="#">
       <p className="bg-gray-200 rounded-full px-3 text-xs py-[2px] text-gray-500 hover:bg-gray-300 hover:text-gray-600 transition-all duration-150 ease-in">Akademia</p>
     </Link>
   )
