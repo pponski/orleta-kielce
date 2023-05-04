2622e976eaa4a6fa56db418a1a61944462ae52fe
pponski <pponski@MacBook-Pro-pponski.local>
2023-05-04 08:07:00 +0200

    article v2

--- src/components/BigArticle.js
+++ src/components/BigArticle.js
@@ -11,7 +11,7 @@ const BigArticle = ({description, image, slug, title}) => {
       <Link href="/pierwszy-post">
     <Image src="https://media.graphassets.com/K9zZGA2UTFG6wviwRocZ" width="920" height="500" className='w-full rounded-md h-60 md:h-80 object-cover object-center md:group-hover:object-bottom transition-all duration-[2s] linear'></Image></Link>
     <Link href="/pierwszy-post">
-    <p className='text-2xl pt-3 font-bold md:group-hover:text-green-700  transition-all duration-100 ease-in'>{title}</p>
+    {title && (<p className='text-2xl pt-3 font-bold md:group-hover:text-green-700  transition-all duration-100 ease-in'>{title}</p>)}
     </Link>
     <div className="info flex items-center gap-4">
     <div className="categories">
