9bd56f4b0b8804508b880442158088c4b0cf3c2b
pponski <pponski@MacBook-Pro-pponski.local>
2023-05-04 08:10:22 +0200

    article v4

--- src/components/SmallArticle.js
+++ src/components/SmallArticle.js
@@ -3,29 +3,25 @@ import Link from 'next/link'
 import React from 'react'
 import CategoryBadge from './CategoryBadge';
 
-const SmallArticle = ({title, slug, image, shortDescription}) => {
+const SmallArticle = ({article}) => {
 
-  if(image){
+  if(article.image){
     var styles = 'single-post mb-4 group cursor-pointer flex items-center gap-0 md:gap-4';
   } else {
     var styles = 'single-post mb-4 group cursor-pointer flex items-center gap-0';
   }
 
-  console.log(title, slug, image, shortDescription);
-
-
-
   return (
     <div className={styles}>
-      {slug && (<Link href={slug} className=''>
-      {image && (<Image src={image.url} width="340" height="200" className='hidden md:block rounded-md h-40 object-cover object-center md:group-hover:object-bottom transition-all duration-[2s] linear'></Image> )}
+      {article.slug && (<Link href={article.slug} className=''>
+      {article.image && (<Image src={article.image.url} width="340" height="200" className='hidden md:block rounded-md h-40 object-cover object-center md:group-hover:object-bottom transition-all duration-[2s] linear'></Image> )}
       </Link>)}
       <div>
-      {slug && (<Link href={slug}>
-        <p class="text-base font-bold group-hover:text-green-700">{title}</p>
+      {article.slug && (<Link href={article.slug}>
+        <p class="text-base font-bold group-hover:text-green-700">{article.title}</p>
         </Link>)}
-        {slug && (<Link href={slug}>
-      {shortDescription && (<p class="text-xs pb-1 max-w-xl font-normal">{shortDescription}</p>)}
+        {article.slug && (<Link href={article.slug}>
+      {article.shortDescription && (<p class="text-xs pb-1 max-w-xl font-normal">{article.shortDescription}</p>)}
       </Link>)}
       <div className="info flex flex-col items-start lg:flex-row lg:gap-4 lg:items-center">
     {/* <div className="categories">
