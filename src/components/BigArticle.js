b77544af95554527c7df24044511814f7f0781b4
pponski <pponski@MacBook-Pro-pponski.local>
2023-05-04 08:08:24 +0200

    article v3

--- src/components/BigArticle.js
+++ src/components/BigArticle.js
@@ -5,13 +5,13 @@ import ButtonPrimary from './ButtonPrimary'
 import CategoryBadge from './CategoryBadge'
 
 
-const BigArticle = ({description, image, slug, title}) => {
+const BigArticle = ({firstArticle}) => {
   return (
     <div className="group cursor-pointer">
       <Link href="/pierwszy-post">
     <Image src="https://media.graphassets.com/K9zZGA2UTFG6wviwRocZ" width="920" height="500" className='w-full rounded-md h-60 md:h-80 object-cover object-center md:group-hover:object-bottom transition-all duration-[2s] linear'></Image></Link>
     <Link href="/pierwszy-post">
-    {title && (<p className='text-2xl pt-3 font-bold md:group-hover:text-green-700  transition-all duration-100 ease-in'>{title}</p>)}
+    {firstArticle.title && (<p className='text-2xl pt-3 font-bold md:group-hover:text-green-700  transition-all duration-100 ease-in'>{firstArticle.title}</p>)}
     </Link>
     <div className="info flex items-center gap-4">
     <div className="categories">
